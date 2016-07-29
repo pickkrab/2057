ba2e05ff28a1dc58563af702cbcc3270c9e43308
AlexanderLukoyanov <pickkrab@gmail.com>
2016-07-29 17:23:58 +0300

    #2

--- src/Solution.java
+++ src/Solution.java
@@ -1,42 +1,56 @@
-/**
- * Created by alexander on 28.07.16.
- */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Solution {
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String c = reader.readLine();
-        int a1 = Integer.parseInt(c);
+        int a = Integer.parseInt(reader.readLine());
 
-        List<Integer> list = new ArrayList<Integer>();
-        for (int k = 0; k < a1; k++) {
-            int a2 = Integer.parseInt(reader.readLine());
-            if (a2 == 1) {
-                list.add(0, Integer.parseInt(reader.readLine()));
-                for (int a = 0; a < list.size() - 1; ++a) {
-                    for (int b = a + 1; b < list.size(); b++) {
-                        if ((list.get(b)).compareTo(list.get(a)) < 0) {
-                            int temp = list.get(a);
-                            list.set(a, list.get(b));
-                            list.set(b, temp);
-                        } else {
-                            continue;
+        Map<Integer, Integer> m = new TreeMap<Integer, Integer>();
+
+        for (int i = 0; i < a; i++) {
+            int b = Integer.parseInt(reader.readLine());
+            if (b == 1) {
+                int o = Integer.parseInt(reader.readLine());
+                if (m.containsKey(o)) {
+                    for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
+                        if (entry.getKey() == o) {
+                            m.put(o, entry.getValue() + 1);
                         }
                     }
+                } else {
+                    m.put(o, 1);
                 }
             }
 
-            if (a2 == 2) {
-                int p = list.get(0);
-                list.remove(0);
-                System.out.println(p);
-            }
-        }
+            if (b == 2) {
 
+                int k = Integer.MAX_VALUE;
+                for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
+                    if (k > entry.getKey()) {
+                        k = entry.getKey();
+                    }
+                }
+                Iterator<Map.Entry<Integer, Integer>> iterator = m.entrySet().iterator();
+                while(iterator.hasNext()) {
+                    Map.Entry<Integer, Integer> pair = iterator.next();
+                    if ((k == pair.getKey()) & (pair.getValue() == 1)) {
+                        System.out.println(pair.getKey());
+                        iterator.remove();
+                    }
+                    if ((k == pair.getKey()) & (pair.getValue() != 1)) {
+                        System.out.println(pair.getKey());
+                        m.put(k, pair.getValue() - 1);
+                    }
+
+                }
+            }
+
+        }
     }
 }
